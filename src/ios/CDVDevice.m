4596c5e6aa4ec58cd27f43331b3ca04a001104cf
Smith <fengfei153@aliyun.com>
2020-07-09 10:48:22 +0800

    增加device.name

--- src/ios/CDVDevice.m
+++ src/ios/CDVDevice.m
@@ -88,6 +88,7 @@
     return @{
              @"manufacturer": @"Apple",
              @"model": [device modelVersion],
+             @"name": [device name],
              @"platform": @"iOS",
              @"version": [device systemVersion],
              @"uuid": [self uniqueAppInstanceIdentifier:device],
